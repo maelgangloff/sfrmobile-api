3bc7b36af80c27260b79ef8162097647e3403b30
Maël Gangloff <contact@maelgangloff.fr>
2022-02-14 20:47:47 +0100

    fix: achatsEnCours type

--- src/entities/AchatsAbonnements.ts
+++ src/entities/AchatsAbonnements.ts
@@ -1,7 +1,20 @@
 export interface AchatsAbonnements {
   debAchats: string
   finAchats: string
-  achatsEnCours: any[]
+  achatsEnCours: Array<{
+    editeur: string
+    prix: string
+    service: string
+    fournisseur: string
+    conteste: boolean
+    smsPlus: boolean
+    dateTransaction: string
+    paiementVia: string
+    refTransaction: string
+    customerServiceContact: string
+    serviceSfr: boolean
+    type: string
+  }>
   achatsHistorique: any[]
   abonnements: any[]
 }
