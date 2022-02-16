4dae9828f1fcc4373a0db4073481d692d6559a7d
Maël GANGLOFF <contact@maelgangloff.fr>
2022-02-16 14:25:15 +0100

    fix: type FicheMonCompte

--- src/entities/FicheMonCompte.ts
+++ src/entities/FicheMonCompte.ts
@@ -11,8 +11,85 @@ export interface FicheMonCompte {
     date: string
   }
   idAsc: string
-  ligneGestionnaire?: FicheMonCompte[]
-  lignesEnGestion?: any[]
+  ligneGestionnaire: Array<{
+    civilite: string
+    commandes: any[]
+    compteWebFixeAdmin: boolean
+    emailDeContactFixe: string
+    emailDeContactMobile: null
+    emailDeSecours: null
+    empreinte: {
+      etat: string
+      date: string
+    }
+    idAsc: string
+    lignesFixes: Array<{
+      boxImg: string
+      crmid: string
+      dateActivation: string
+      dateCreation: string
+      decodeurImg: string
+      decodeurTV: boolean
+      facturationData: {
+        adresse: Adresse
+      }
+      gamme: string
+      isNumericable: boolean
+      libelleCommercialBox: string
+      libelleCommercialDecodeur: string
+      libelleOffreSouscrite: string
+      multipackId: string
+      multipackRemiseGroupe: number
+      multipackRemiseLigne: number
+      ndi: string
+      optionsSouscrites: Array<{
+        codeOption: string
+        libelle: null | string
+      }>
+      profilPSW: string
+      statutJoya: string
+      technoLigne: string
+      utilisateurData: {
+        adresse: Adresse
+      }
+    }>
+    lignesMobiles: Array<{
+      box4G: boolean
+      facturationData: {
+        adresse: Adresse
+      }
+      gammeTarifaire: string
+      libelleForfait: string
+      msisdn: string
+      multipackId: null
+      multipackRemiseGroupe: null
+      multipackRemiseLigne: null
+      numeroContrat: string
+      offreYellow: null
+      optionsSouscrites: Array<{
+        codeProduit: string
+        libelleCourt: string
+      }>
+      profilPSW: string
+      segment: string
+      statutJoya: null
+      telephoneMobileIdBacarat: null
+      telephoneMobileImei: string
+      telephoneMobileTac: string
+      utilisateurData: {
+        adresse: Adresse
+      }
+    }>
+    logins: string[]
+    may: boolean
+    nom: string
+    prenom: string
+    srr: boolean
+    telephoneFixeDeContact: string
+    telephoneMobileDeContact: string
+    telephoneMobileDeSecours: string
+  }>
+  lignesEnGestion: any[]
   lignesFixes: Array<{
     boxImg: string
     crmid: string
