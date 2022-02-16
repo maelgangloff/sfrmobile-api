4dc931e068a0c46b02c3c93f73675c106a070ca7
Maël GANGLOFF <contact@maelgangloff.fr>
2022-02-16 17:51:24 +0100

    fix: type FicheMonCompte

--- src/entities/FicheMonCompte.ts
+++ src/entities/FicheMonCompte.ts
@@ -1,6 +1,6 @@
 export interface FicheMonCompte {
   civilite: string
-  cogeres: any[]
+  cogeres?: any[]
   commandes: any[]
   compteWebFixeAdmin: boolean | null
   emailDeContactFixe: null | string
@@ -11,85 +11,8 @@ export interface FicheMonCompte {
     date: string
   }
   idAsc: string
-  ligneGestionnaire: Array<{
-    civilite: string
-    commandes: any[]
-    compteWebFixeAdmin: boolean
-    emailDeContactFixe: string
-    emailDeContactMobile: null
-    emailDeSecours: null
-    empreinte: {
-      etat: string
-      date: string
-    }
-    idAsc: string
-    lignesFixes: Array<{
-      boxImg: string
-      crmid: string
-      dateActivation: string
-      dateCreation: string
-      decodeurImg: string
-      decodeurTV: boolean
-      facturationData: {
-        adresse: Adresse
-      }
-      gamme: string
-      isNumericable: boolean
-      libelleCommercialBox: string
-      libelleCommercialDecodeur: string
-      libelleOffreSouscrite: string
-      multipackId: string
-      multipackRemiseGroupe: number
-      multipackRemiseLigne: number
-      ndi: string
-      optionsSouscrites: Array<{
-        codeOption: string
-        libelle: null | string
-      }>
-      profilPSW: string
-      statutJoya: string
-      technoLigne: string
-      utilisateurData: {
-        adresse: Adresse
-      }
-    }>
-    lignesMobiles: Array<{
-      box4G: boolean
-      facturationData: {
-        adresse: Adresse
-      }
-      gammeTarifaire: string
-      libelleForfait: string
-      msisdn: string
-      multipackId: null
-      multipackRemiseGroupe: null
-      multipackRemiseLigne: null
-      numeroContrat: string
-      offreYellow: null
-      optionsSouscrites: Array<{
-        codeProduit: string
-        libelleCourt: string
-      }>
-      profilPSW: string
-      segment: string
-      statutJoya: null
-      telephoneMobileIdBacarat: null
-      telephoneMobileImei: string
-      telephoneMobileTac: string
-      utilisateurData: {
-        adresse: Adresse
-      }
-    }>
-    logins: string[]
-    may: boolean
-    nom: string
-    prenom: string
-    srr: boolean
-    telephoneFixeDeContact: string
-    telephoneMobileDeContact: string
-    telephoneMobileDeSecours: string
-  }>
-  lignesEnGestion: any[]
+  ligneGestionnaire?: FicheMonCompte[]
+  lignesEnGestion?: FicheMonCompte[]
   lignesFixes: Array<{
     boxImg: string
     crmid: string
@@ -128,12 +51,15 @@ export interface FicheMonCompte {
     gammeTarifaire: string
     libelleForfait: string
     msisdn: string
-    multipackId: null
-    multipackRemiseGroupe: null
-    multipackRemiseLigne: null
+    multipackId: null | string
+    multipackRemiseGroupe: number | null
+    multipackRemiseLigne: number | null
     numeroContrat: string
-    offreYellow: null
+    offreYellow: null | string
     optionsSouscrites: Array<{
+      codeOption: string
+      libelle: null | string
+    }> | Array<{
       codeProduit: string
       libelleCourt: string
     }>
